2f13b598284da78e727e7ce59628f900a3305542
shanehull <hello@shanehull.com>
2024-09-04 23:30:25 +1000

    fix: fully load index, defer scripts and use jsDelivr

--- internal/handlers/index.go
+++ internal/handlers/index.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"debtrecyclingcalc.com/internal/buildinfo"
+	"debtrecyclingcalc.com/internal/calc"
+	"debtrecyclingcalc.com/internal/charts"
 	"debtrecyclingcalc.com/internal/templates"
 )
 
@@ -26,13 +28,50 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	params := &calc.DebtRecyclingParameters{
+		Salary:               150000,
+		InitialInvestment:    100000,
+		AnnualInvestment:     50000,
+		MortgageSize:         600000,
+		MortgageInterestRate: 0.05,
+		CapitalGrowthRate:    0.08,
+		NumYears:             10,
+		Country:              "au",
+		ReinvestDividends:    true,
+		ReinvestTaxRefunds:   true,
+	}
+
+	data, err := calc.DebtRecycling(*params)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	positionsChart, err := charts.Positions(data, params.NumYears, r.Context())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	incomeChart, err := charts.Income(data, params.NumYears, r.Context())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	interestChart, err := charts.Interest(data, params.NumYears, r.Context())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	index := templates.Index(
 		templates.Hero(),
 		templates.Form(),
-		templates.Blank(),
+		templates.Results(data, params, positionsChart, incomeChart, interestChart),
 	)
 
-	err := templates.Layout(index, "Debt Recycling Calculator", buildinfo.GitTag, buildinfo.BuildDate).
+	err = templates.Layout(index, "Debt Recycling Calculator", buildinfo.GitTag, buildinfo.BuildDate).
 		Render(r.Context(), w)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
